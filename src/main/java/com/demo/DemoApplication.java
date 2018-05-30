77157a046426e213bb04d02347cd7321c730eb2c
user <tuanru9166@163.com>
2018-05-30 17:47:24 +0800

    test

--- src/main/java/com/demo/DemoApplication.java
+++ src/main/java/com/demo/DemoApplication.java
@@ -1,9 +1,13 @@
 package com.demo;
 
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import tk.mybatis.spring.annotation.MapperScan;
+
 
 @SpringBootApplication
+@MapperScan(basePackages = "com.demo.mapper")
 public class DemoApplication {
 
 	public static void main(String[] args) {
